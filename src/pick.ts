4ea70533aee51d23782660adbd6def4b257a7243
Ayon Lee <ayonlys@gmail.com>
2020-02-12 16:01:34 +0800

    add pick()

---
+++ src/pick.ts
@@ -0,0 +1,14 @@
+/** Creates an array composed of the picked elements. */
+export default function pick<T extends any[]>(arr: T, indexes: number[]): T;
+/** Creates an object composed of the picked properties. */
+export default function pick<T extends object, U extends keyof T>(obj: T, props: (U | symbol)[]): Pick<T, U>;
+export default function pick(obj: any, props: (string | number | symbol)[]) {
+    if (Array.isArray(obj)) {
+        return (<number[]>props).map(i => obj[i]);
+    } else {
+        return props.reduce((result: any, prop: string) => {
+            prop in obj && (result[prop] = obj[prop]);
+            return result;
+        }, Object.create(Object.getPrototypeOf(obj)));
+    }
+}
---
+++ test/pick.js
@@ -0,0 +1,23 @@
+/* global describe, it */
+const assert = require("assert");
+const pick = require("../pick").default;
+
+describe("pick", () => {
+    it("should pick properties from the object", () => {
+        let err = new Error("Something went wrong");
+        let foo = Symbol("foo");
+        err[foo] = "Hello, World!";
+        let result = pick(err, ["name", "message", foo]);
+
+        assert(result instanceof Error);
+        assert.deepStrictEqual({ ...result }, {
+            name: err.name,
+            message: err.message,
+            [foo]: err[foo]
+        });
+    });
+
+    it("should pick elements from the array", () => {
+        assert.deepStrictEqual(pick([1, 2, 3, 4, 5], [2, 3]), [3, 4]);
+    });
+});
