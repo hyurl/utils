fdeaff262ff04ec758f4f2564d3d633758f1326b
Ayon Lee <i@hyurl.com>
2020-04-22 15:04:59 +0800

    commit webpack config file to repo

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    mode: "production",
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    target: "node",
+    node: {
+        process: false
+    },
+    output: {
+        path: __dirname + "/bundle",
+        filename: "hyurl-utils.min.js",
+        library: "@hyurl/utils",
+        libraryTarget: "umd",
+        globalObject: "this",
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                loader: "ts-loader",
+                options: {
+                    configFile: __dirname + "/webpack-tsconfig.json"
+                }
+            }
+        ]
+    }
+};
